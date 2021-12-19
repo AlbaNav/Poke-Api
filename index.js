08005afa9897b6e9325e8ba1b001b582e824d492
Alba <alba.m.navarro@gmail.com>
2021-12-19 22:49:07 +0100

    feat formato

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const div$$ = document.querySelector(".container");
 const arrayPoke = () => {
   const allPoke = [];
-
+    
   for (let i = 1; i <= 151; i++) {
-    const baseDatos = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    const baseDatos =  `https://pokeapi.co/api/v2/pokemon/${i}`;
     allPoke.push(fetch(baseDatos).then((res) => res.json()));
   }
 
@@ -14,15 +14,14 @@ const arrayPoke = () => {
 
 const print = (pokemon) => {
   for (const poke of pokemon) {
-    const div2$$ = document.createElement("div");
-    div2$$.innerHTML = `<h3> name: ${poke.name}</h3>
-    <img src="${poke.sprites["front_default"]}" max-width="200px"/>
-    <p>ID: ${poke.id}</p> <p> Experiencia: ${
-      poke.base_experience
-    }</p><p> Height: ${poke.height / 10} M</p><p>Weight: ${
-      poke.weight / 10
-    } KG</p>`;
-    div$$.appendChild(div2$$);
+    const ul$$ = document.createElement("ul");
+    ul$$.innerHTML =`<li><h3 id="contenido"> name: ${poke.name}</h3></li>
+         <li><img id="contenido" src="${poke.sprites["front_default"]}"/></li>
+         <li><p id="contenido">ID: ${poke.id}</p></li>
+         <li>Experiencia: ${poke.base_experience}</li>
+         <li>Height: ${poke.height / 10} M</li>
+         <li>Weight: ${poke.weight / 10} KG</p></li>`
+    div$$.appendChild(ul$$);
   }
 };
 
