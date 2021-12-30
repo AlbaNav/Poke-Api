94ff64bd2e5012f81606a267dca7d18ae819969f
Alba <alba.m.navarro@gmail.com>
2021-12-30 19:40:32 +0100

    responsive final

--- index.js
+++ index.js
@@ -15,13 +15,10 @@ const arrayPoke = () => {
     poke.name.toLowerCase().includes(input$$.value.toLowerCase())||
     poke.id==(input$$.value));
     print(filteredCharacters);
-  
-    
-    
+   
   });
   
 };
-
 const print = (allPoke) => { 
     div$$.innerHTML="";
     for (const poke of allPoke) {
@@ -29,16 +26,12 @@ const print = (allPoke) => {
     ul$$.innerHTML =`<li><h3 id="contenido"> name: ${poke.name}</h3></li>
          <li><img id="contenido" src="${poke.sprites["front_default"]}"/></li>
          <li><p id="contenido">ID: ${poke.id}</p></li>
-         <li>Experiencia: ${poke.base_experience}</li>
+         <li>Experience: ${poke.base_experience}</li>
          <li>Height: ${poke.height / 10} M</li>
          <li>Weight: ${poke.weight / 10} KG</p></li>`
     div$$.appendChild(ul$$);
-
-
-
   }
 };
-
 
 arrayPoke();
 
